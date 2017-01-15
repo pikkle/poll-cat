3bc13744432114c4715a716445a95488d9ea94b2
Julien Leroy <julien.leroy@limayankee.com>
2017-01-15 14:36:19 +0100

    add some endpoints for creating polls and questions for a room

--- server/poll_cat/api/views.py
+++ server/poll_cat/api/views.py
@@ -1,7 +1,4 @@
 from channels import Group
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from uuid import uuid4
@@ -15,17 +12,62 @@ class Rooms (APIView):
         json = JSONParser().parse(request)
         uuid = str(uuid4())[:6]
         token = str(uuid4())[:18]
-        room = Room(title=json['title'], uuid=uuid, connected=0, token=token)
+        room = Room(title=json['title'], number=uuid, connected=0, token=token)
+        room.save()
         request.session['room_admin_uuid'] = uuid
 
         return Response({'token': token, 'room': uuid}, status=201)
 
-class Question (APIView):
-    def post(self, request):
-        room = 111111
-        Group("room-%s" % 111111).send({
-            "text": 'ok'
-        })
 
-        return Response({}, status=201)
+class Auth (APIView):
+    def post(self, request):
+        json = JSONParser().parse(request)
+
+        room = Room.objects.filter(token=json['token']).first()
+
+        if room:
+            request.session['room_admin_uuid'] = room.number
+            return Response({}, status=200)
+
+        else:
+            return Response({'error': 'this token is invalid'}, status=403)
+
+
+
+
+class Questions (APIView):
+    def post(self, request, room_number):
+        room = Room.objects.filter(number=room_number).first()
+
+        if room:
+            Group("room-%s" % room_number).send({
+                "text": 'new question in the room'
+            })
+
+            return Response({}, status=201)
+
+        else:
+            return Response({"error": "no room " + room.number + " found"}, status=404)
+
+
+class Polls (APIView):
+    def post(self, request, room_number):
+
+        if 'room_admin_uuid' not in request.session or request.session['room_admin_uuid'] != room_number:
+            return Response({"error": "you can't post polls for this room"}, status=401)
+
+        else:
+            room = Room.objects.filter(number=room_number).first()
+
+            if room:
+                Group("room-%s" % room_number).send({
+                    "text": 'new poll in the room'
+                })
+
+                return Response({}, status=201)
+
+            else:
+                return Response({"error": "no room " + room.number + " found"}, status=404)
+
+
 
